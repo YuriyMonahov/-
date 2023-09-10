9a1998d330e066804861100b82b229e49c5e01fa
Yura <ya.monakh771@gmail.com>
2023-09-10 18:22:15 +0300

    add is/else

---
+++ Example005/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите имя ");
+string name = Console.ReadLine();
+if (name == "Юрий")
+{
+    Console.WriteLine("Ура, это Юра");
+}
+else
+{
+    Console.WriteLine("Привет, "+ name);
+}
