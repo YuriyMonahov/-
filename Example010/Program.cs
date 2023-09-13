863bb91e451b621fc72e18d44e05e957525dc1b2
Yura <ya.monakh771@gmail.com>
2023-09-13 21:35:37 +0300

    add while

---
+++ Example010/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = {14,26,36,48,59,69,87,83,};
+int n = array.Length;
+int find = 59;
+int index = 0;
+ while (index < n)
+ {
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+ }
